d8b398c5c838df3d7b4a4ca516b5eec8c168ef0b
ducbang1510 <1851050006bang@ou.edu.vn>
2021-07-19 17:09:37 +0700

    update admin

--- ecourses/courses/admin.py
+++ ecourses/courses/admin.py
@@ -1,7 +1,23 @@
 from django.contrib import admin
+from django.utils.html import mark_safe
 from .models import Lesson, Course, Category, Tag
 
+
+class LessonAdmin(admin.ModelAdmin):
+    class Media:
+        css = {
+            'all': ('/static/css/main.css', )
+        }
+    list_display = ['id', 'subject', 'created_date', 'active', 'course']
+    search_fields = ['subject', 'created_date', 'course__subject']
+    list_filter = ['subject', 'course__subject']
+    readonly_fields = ['avatar']
+
+    def avatar(self, lesson):
+        return mark_safe("<img src='/static/{img_url}' alt='{alt}' width=120px />".format(img_url=lesson.image.name, alt=lesson.subject))
+
+
 # Register your models here.
-admin.site.register(Lesson)
+admin.site.register(Lesson, LessonAdmin)
 admin.site.register(Course)
 admin.site.register(Category)
